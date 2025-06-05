113426b0dc8fbb5c7b0b001f3b95d1bd23a3158a
Khoanh Chan Kam Li <trancamly06@gmail.com>
2025-06-05 16:16:39 +0200

    Create graph_generation_k1.py

---
+++ graph_generation_k1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def generate_matrices(SEED=4329, NUM_NODES=12):
+    np.random.seed(SEED)
+    random_matrix = 2.0 * np.random.rand(NUM_NODES, NUM_NODES)
+
+
+    coeff_k = 1.0 - int(str(SEED)[2]) * 0.01 - int(str(SEED)[3]) * 0.01 - 0.3
+    adjacency_matrix = (random_matrix * coeff_k >= 1.0).astype(int)
+
+
+    adjacency_matrix_undir = adjacency_matrix + adjacency_matrix.T
+    adjacency_matrix_undir[adjacency_matrix_undir > 1] = 1
+
+
+    return adjacency_matrix, adjacency_matrix_undir
+
+
+if __name__ == "__main__":
+    adj_matrix_dir, adj_matrix_undir = generate_matrices()
+    print("Directed Adjacency Matrix:\n", adj_matrix_dir)
+    print("\nUndirected Adjacency Matrix:\n", adj_matrix_undir)
